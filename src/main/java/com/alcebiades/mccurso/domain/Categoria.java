ace564554a754175f17bb65b5ffcea049e3d4538
Alcebiades Gamas <alcebiades.29@gmail.com>
2020-10-20 12:35:13 -0300

    Classe de Dominio Categoria Testada.

---
+++ src/main/java/com/alcebiades/mccurso/domain/Categoria.java
@@ -0,0 +1,63 @@
+package com.alcebiades.mccurso.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	private Integer idProduto;
+	private  String nome;
+	
+	public Categoria() {
+		
+	}
+	
+	public Categoria(Integer idProduto, String nome) {
+		super();
+		this.idProduto = idProduto;
+		this.nome = nome;
+	}
+
+	public Integer getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(Integer idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idProduto == null) ? 0 : idProduto.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (idProduto == null) {
+			if (other.idProduto != null)
+				return false;
+		} else if (!idProduto.equals(other.idProduto))
+			return false;
+		return true;
+	}
+	
+	
+}
