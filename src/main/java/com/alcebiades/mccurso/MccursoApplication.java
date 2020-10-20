79c1be076b640151cbec803fe5de7a70b624c445
Alcebiades Gamas <alcebiades.29@gmail.com>
2020-10-20 11:35:42 -0300

    Projeto Criado

---
+++ src/main/java/com/alcebiades/mccurso/MccursoApplication.java
@@ -0,0 +1,13 @@
+package com.alcebiades.mccurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MccursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MccursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alcebiades/mccurso/MccursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alcebiades.mccurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
